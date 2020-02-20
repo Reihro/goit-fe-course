449567a3f1c6d97b0a27833e4112bb5795d4f854
Reihro <47672262+Reihro@users.noreply.github.com>
2020-02-21 01:09:01 +0200

    Add files via upload

--- javascript-homework/goit-js-hw-12-countries/src/index.js
+++ javascript-homework/goit-js-hw-12-countries/src/index.js
@@ -16,49 +16,34 @@ const refs = {
 // "lodash.debounce": "^4.0.8",
 
 // но и сделать import
-import { debounce } from 'lodash';
-debounce = require('lodash.debounce');
+import { debounce } from "lodash";
+debounce = require("lodash.debounce");
 // И тогда
 refs.country.addEventListener("input", debounce(searchCountry, 500));
 
-// let debounced = _.debounce(searchCountry, 500);
-// refs.country.addEventListener("input", debounced);
-
 // INSTRUMENTS
-function buildList(items) {
-  return resultUl(items);
+function buildList(data) {
+  return resultUl(data);
 }
-function buildCard(items) {
-  return resultCard(items);
-}
-function insertList(markup) {
-  refs.countriesResult.insertAdjacentHTML("beforeend", markup);
-}
-function insertCard(markup) {
-  refs.countryResult.insertAdjacentHTML("beforeend", markup);
-}
-function clearCard() {
-  refs.countryResult.innerHTML = "";
-}
-function clearList() {
-  refs.countriesResult.innerHTML = "";
+function buildCard(data) {
+  return resultCard(data);
 }
 
 //SEARCH
 function searchCountry(e) {
-  clearCard();
-  clearList();
+  refs.countryResult.innerHTML = "";
+  refs.countriesResult.innerHTML = "";
   e.preventDefault();
   let searchQuery = e.target.value;
   if (searchQuery != "") {
     service.fetchCountry(searchQuery).then(data => {
       if (data.length === 1) {
         const markup = buildCard(data);
-        insertCard(markup);
+        refs.countryResult.insertAdjacentHTML("beforeend", markup);
       }
       if (10 >= data.length >= 1) {
         const markup = buildList(data);
-        insertList(markup);
+        refs.countriesResult.insertAdjacentHTML("beforeend", markup);
       }
       if (data.length > 10) {
         PNotify.error({
