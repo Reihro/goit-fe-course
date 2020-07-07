f75b8706848bb6c2bc0d1505be5104fda03a49b5
Reihro <47672262+Reihro@users.noreply.github.com>
2020-07-07 18:18:48 +0300

    Add files via upload

---
+++ react-hw/goit-react-hw-01-components/build/precache-manifest.433949ae7b9c4c9e43173fc36585efab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac1fd478c1f5fc4d5f4479607b099c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9065faab8c895ac4723",
+    "url": "/static/css/main.baf3617f.chunk.css"
+  },
+  {
+    "revision": "bb9b60a14fc34bc4cbbf",
+    "url": "/static/js/2.2efb1924.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2efb1924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9065faab8c895ac4723",
+    "url": "/static/js/main.042caecb.chunk.js"
+  },
+  {
+    "revision": "3fd7c41e85fe20a69193",
+    "url": "/static/js/runtime-main.1a265c15.js"
+  }
+]);
